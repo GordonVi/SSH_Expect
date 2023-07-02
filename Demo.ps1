bc764c805d59bbe6effeb003efd64a67e8855f83
Gordon Virasawmi <57081843+GordonVi@users.noreply.github.com>
2023-07-02 19:19:33 -0400

    Initial Release
    Tested on Win11 Home and Debian Server (offsite)

---
+++ Demo.ps1
@@ -0,0 +1,78 @@
+. .\SSH_Expect_Functions
+
+# SSH Credentials
+$site = "virasawmi.com"
+$user = "username"
+$password = ConvertTo-SecureString "Password" -AsPlainText -Force
+
+$credential = New-Object PSCredential ($user, $password)
+
+# Save to flat file
+# $credential |  Export-Clixml -Path .\fakesite.com.xml
+
+# Load from flat file - This is to obscure passwords from git
+$credential = Import-Clixml -Path fakesite.com.xml
+
+# -------------------------------
+
+
+# create session on this client
+$Session = New-SSHSession -ComputerName $site -Credential $credential -AcceptKey
+
+
+# -----------------------------------------------
+
+if ($session.host.length -gt 0) {
+	
+	# connect to host
+	# Note: Do not change this variable name. The Included Function require this name as to access it globally.
+	$SSHStream = New-SSHShellStream -Index $Session.SessionId
+
+# -------------------------------
+
+	$SSHStream.read() | Out-Null # Clear the SSH read buffer. Usually, servers have ASCII art as a server header.
+
+	# Demo section
+
+		ssh_linux_command_display "ping -c 2 google.com"
+		ssh_linux_command_display "traceroute google.com"
+
+		ssh_prompt_command_display "$" "ping -c 2 google.com"
+
+	"Starting Expect Commands..."
+
+		ssh_expect_command "read -p `"User: `" uservar" "User:" "Test1" | out-null
+		ssh_expect_command "read -p `"Pass: `" passvar" "Pass:" "Test2" | out-null
+		ssh_expect_command "read -p `"Test: `" testvar; read -p `"Tester: `" testervar; read -p `"Testerx: `" testerxvar" "Test:" "Test3" | out-null
+
+		ssh_expect "Tester" "Test4" | out-null
+		ssh_expect "Testerx" "Test5" | out-null
+
+	"Ending Expect Commands..."
+	
+	"Starting ping wait..."
+	
+		$SSHStream.writeline("ping -c 4 google.com")
+		ssh_wait "min/avg/max/mdev"
+		
+	"Ending ping wait..."
+	
+
+		sleep 1 # Powershell is faster than the SSH client's refresh. This is to give SSH some time to catch up and update the SSH buffer.
+		
+		ssh_linux_command_display "echo uservar = `$uservar"
+		ssh_linux_command_display "echo passvar = `$passvar"
+		ssh_linux_command_display "echo testvar = `$testvar"
+		ssh_linux_command_display "echo testervar = `$testervar"
+		ssh_linux_command_display "echo testerxvar = `$testerxvar"
+
+		
+# -------------------------------
+
+	# close and remove SSH session
+	$SSHStream.Close()
+	Remove-SSHSession -SSHSession $session | Out-Null
+	
+} else {write-host -foregroundcolor yellow "no connection"}
+
+
---
+++ SSH_Expect_Functions.ps1
@@ -0,0 +1,140 @@
+function global:ssh_wait($expect){
+
+		
+		$expectVar = 0
+		$lines=@()
+		$total=@()
+		do {
+
+			sleep 1
+			do {} until ($SSHStream.dataavailable)
+		
+			$lines = foreach ($line in $($SSHStream.read()).split("`n")) {if ($line -like "*$expect*") {$expectvar=1}}
+			
+		} until ($expectvar -eq 1)
+		
+}
+
+function global:ssh_expect($expect, $answer){
+
+		
+		do {} until ($SSHStream.dataavailable)
+		sleep 1
+
+		$expectvar=0
+		$lines=@()
+		$total=@()
+		do {
+
+			
+			if ($SSHStream.dataavailable) {
+				
+				$y = $($SSHStream.Read()).split("`n")
+				$x = $y[$y.count-1]
+				
+				#foreach ($x in $y) {
+					if ($x -like "*$expect*") {$SSHStream.writeline($answer) ; $expectvar=1}
+				#}
+			}
+
+
+
+		} until ($expectvar -eq 1 -or $SSHStream.dataavailable -eq $false)
+		
+		if ($expectvar -eq 0) {return 0} else {return 1}
+		
+}
+
+function global:ssh_expect_command($command, $expect, $answer){
+
+		
+		do {$SSHStream.read() | out-null} until ($SSHStream.dataavailable -eq $false)
+		$SSHStream.writeline($command)
+		do {} until ($SSHStream.dataavailable)
+		sleep 1
+
+		$expectvar=0
+		$lines=@()
+		$total=@()
+		do {
+
+			
+			if ($SSHStream.dataavailable) {
+				
+				$y = $($SSHStream.Read()).split("`n")
+				foreach ($x in $y) {
+					if ($x -like "*$expect*" -and $x -notlike "*$command*") {$SSHStream.writeline($answer) ; $expectvar=1}
+				}
+			}
+
+
+
+		} until ($expectvar -eq 1 -or $SSHStream.dataavailable -eq $false)
+
+		do {$SSHStream.read() | out-null} until ($SSHStream.dataavailable -eq $false)
+		if ($expectvar -eq 0) {return 0} else {return 1}
+		
+}
+
+function global:ssh_prompt_command($prompt,$command){
+
+		
+		do {$SSHStream.read() | out-null} until ($SSHStream.dataavailable -eq $false)
+		$SSHStream.writeline($command)
+
+		$lines=@()
+		$total=@()
+		do {
+
+			sleep 1
+			do {} until ($SSHStream.dataavailable)
+		
+			$lines = foreach ($line in $($SSHStream.read()).split("`n")) {if ($line -ne "") {$line}}
+			$total += $lines
+			
+		} until ($lines[$lines.count-1] -like "*$prompt*")
+		
+		return $total
+		
+}
+
+function global:ssh_prompt_command_display($prompt, $command){
+
+		$StopWatch = New-Object System.Diagnostics.Stopwatch
+		$StopWatch.Start()
+		write-host -foregroundcolor yellow "$prompt > $command"
+		$lines=$(ssh_prompt_command $prompt $command)
+		$StopWatch.Stop()
+
+		write-host -foregroundcolor green "Time: $($StopWatch.Elapsed)"
+		foreach ($line in $lines) {write-host -foregroundcolor cyan $line}
+	
+}
+
+function global:ssh_linux_command($command){
+
+		$sentinal = "UniqueScriptSentinal"
+		$line=""
+		$lines = @()
+
+		do {$SSHStream.read() | out-null} until ($SSHStream.dataavailable -eq $false)
+		$SSHStream.writeline($command)
+		$SSHStream.writeline("echo $sentinal")
+		do {$line = $SSHStream.ReadLine(); if ($line -notlike "*$sentinal*" -and $line -notlike "*$command*") {$lines += "$line"}} while ($line -notlike "*$sentinal*")
+
+		return $lines
+}
+
+
+function global:ssh_linux_command_display($command){
+
+		$StopWatch = New-Object System.Diagnostics.Stopwatch
+		$StopWatch.Start()
+		write-host -foregroundcolor yellow $command
+		$lines=$(ssh_linux_command($command))
+		$StopWatch.Stop()
+
+		write-host -foregroundcolor green "Time: $($StopWatch.Elapsed)"
+		foreach ($line in $lines) {write-host -foregroundcolor cyan $line}
+	
+}
